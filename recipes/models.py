89461d26345a8f4beafe1c78036e0e92cea6a7e5
Mrunalee Patel <mrunaleepatel997@gmail.com>
2023-09-20 19:25:16 -0500

    updated model and readme

--- recipes/models.py
+++ recipes/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 class Recipe(models.Model):
     title = models.CharField(max_length=200)
-    description = models.TextField()
+    img = models.ImageField()
+    url = models.TextField()
     author = models.CharField(max_length=200)
     course = models.CharField(max_length=100)
