3447e721c65879635f70cfba847d4aa64155d7aa
Mrunalee Patel <mrunaleepatel997@gmail.com>
2023-09-23 10:39:16 -0500

    field change

--- recipes/serializers.py
+++ recipes/serializers.py
@@ -4,4 +4,4 @@ from rest_framework import serializers
 class RecipeSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Recipe
-        fields = ('title', 'img', 'url', 'author', 'course')
+        fields = ('id', 'title', 'img', 'url', 'author', 'course')
